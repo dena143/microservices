a5e4cce664b2e5b7be5fe24abe70b5fe5c6a0c6d
Mochamad Dena Eka Putra <sica4822@gmail.com>
2022-01-11 21:30:04 +0700

    day 2, make post, comment, and event-bus

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentByPostId = {};
 
@@ -11,7 +14,7 @@ app.get("/post/:id/comments", (req, res) => {
   res.status(200).send(commentByPostId[req.params.id] || []);
 });
 
-app.post("/post/:id/comments", (req, res) => {
+app.post("/post/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -21,7 +24,22 @@ app.post("/post/:id/comments", (req, res) => {
 
   commentByPostId[req.params.id] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event:", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
